671c63bdc2f02012129647286f83ec4120bf528a
SarahG236 <sarah.gilmore@bridgebss.com>
2026-02-24 18:28:10 -0500

    Initial Commit

---
+++ Am I Outside?/Am_I_Outside_App.swift
@@ -0,0 +1,17 @@
+//
+//  Am_I_Outside_App.swift
+//  Am I Outside?
+//
+//  Created by Sarah Gilmore on 2/24/26.
+//
+
+import SwiftUI
+
+@main
+struct Am_I_Outside_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
